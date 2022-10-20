918992f1dff311a1886d4307d5c810601644acf4
Akula_tony <konivchenko94@gmail.com>
2022-10-20 14:44:48 +0300

    Задача №10

---
+++ HWLess2/HW1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+
+Console.Clear();
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+int a = (number / 10) % 10;
+Console.WriteLine($"{a}");
