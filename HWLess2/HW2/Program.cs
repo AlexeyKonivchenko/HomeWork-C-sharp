0fa40d4cba645464deacaf4fe7db202a009dc30e
Akula_tony <konivchenko94@gmail.com>
2022-10-20 15:16:01 +0300

    решение задачи № 13

---
+++ HWLess2/HW2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+Console.Clear();
+
+
+Console.WriteLine("Генерируем случайное число!");
+int number = new Random().Next(10, 1000);
+Console.WriteLine(number);
+if (number > 100)
+{
+    int num = (number % 10);
+    Console.WriteLine($"Третья цифра числа = {num}");
+}
+else 
+{
+Console.WriteLine("Число двухзначное");
+}
