d385b664b880d984b52f2565754bbc5daa259279
Akula_tony <konivchenko94@gmail.com>
2022-11-09 13:31:42 +0300

    Задача № 27

---
+++ HWLess4/task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+
+int num = int.Parse(Console.ReadLine());
+int sum = 0;
+for (int i = 3; i >= 0; i--)
+{
+    sum += num / (int)Math.Pow(10.0, i);
+    num = num % (int)Math.Pow(10.0, i);
+}
+Console.WriteLine($"Сумма цифр введенного числа = {sum}");
+
+
