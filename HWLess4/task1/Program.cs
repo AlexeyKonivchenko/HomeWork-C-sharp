7a1a4b4a33ad58cdbe9cd5a309e02fe43204e4cf
Akula_tony <konivchenko94@gmail.com>
2022-11-10 22:07:47 +0300

    Задачи с семинара

---
+++ HWLess4/task1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+
+Console.Clear();
+
+void Pow(int numberA, int numberB)
+{
+    int result = numberA;
+    if (numberB == 0)
+        result = 1;
+
+    else if (numberB > 1)
+    {
+        for (int i = 2; i <= numberB; i++)
+        {
+            result *= numberA;
+        }
+    }
+    Console.WriteLine($"Number {numberA} to the power of {numberB} is {result}.");
+}
+
+Console.WriteLine("Please enter first number: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter second number: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+if (B < 0)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Second number is incorrect: it must must be 0 or greater.");
+
+}
+else
+{
+    Pow(A, B);
+}
--- example/Program.cs
+++ example/Program.cs
@@ -209,7 +209,7 @@
 //     }
 //     else
 //     {
-        // Console.WriteLine("Введите число больше 1");
+// Console.WriteLine("Введите число больше 1");
 //     }
 // }
 // Console.WriteLine("enter number");
@@ -276,3 +276,143 @@
 // https://qna.habr.com/q/519776
 // https://docs.google.com/presentation/d/1a61-mEq8z9CS2XRe5mC-egYhwOX5AYsz9cLDL5fkwbk/edit#slide=id.g11745f219f0_0_21
 
+
+
+// int[] CreateRandomArray(int N, int start, int end)
+// {
+//     int[] RandomArray = new int[N];
+//     for (int i = 0; i < N; i++)
+//     {
+//         RandomArray[i] = new Random().Next(start, end + 1);
+//     }
+//     return RandomArray;
+// }
+
+// int[] CreateArray()
+// {
+//     Console.WriteLine("Введите количество элементов массива");
+//     int size = Convert.ToInt32(Console.ReadLine());
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {   
+//         Console.WriteLine($"Введите {i+1} элемент массива");
+//         array[i] = Convert.ToInt32(Console.ReadLine());
+//     }
+//     return array;
+// }
+
+
+// void ShowArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+
+
+
+
+
+// int[] myArray = CreateRandomArray(12, -9, 9);
+// ShowArray(myArray);
+
+// int sum_pos = 0;
+// int sum_neg = 0;
+
+// for (int i = 0; i < myArray.Length; i++)
+// {
+//     if (myArray[i] > 0)
+//         sum_pos += myArray[i];
+//     else
+//         sum_neg += myArray[i];
+// }
+
+// Console.WriteLine($"Сумма положительных: {sum_pos}. Сумма отрицательных: {sum_neg}");
+
+
+
+// https://learn.microsoft.com/ru-ru/dotnet/api/system.random.next?view=net-7.0
+
+
+
+// Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+
+
+// int[] CreateRandomArray(int N, int start, int end)
+// {
+//     int[] RandomArray = new int[N];
+//     for (int i = 0; i < N; i++)
+//     {
+//         RandomArray[i] = new Random().Next(start, end + 1);
+//     }
+//     return RandomArray;
+// }
+
+// int[] array = CreateRandomArray(10 , -100 , 100);
+// Console.WriteLine($"{string.Join(" ", array)}");
+// for (int i = 0; i < array.Length; i++)
+// {
+//         array[i]= array[i] * -1;
+// }
+// Console.WriteLine($"{string.Join(" ", array)}");
+
+
+// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+// int[] array = CreateRandomArray(10, -100, 100);
+// Console.WriteLine($"{string.Join(" ", array)}");
+// Console.WriteLine("Введите число: ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// bool serge = false;
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if (array[i] == x)
+//     {
+//         serge = true;
+//     }
+
+
+// }
+// Console.WriteLine(serge?"Да":"нет");
+
+
+// Задача 35: Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. 
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+
+
+// int[] array = CreateRandomArray(123, -200, 200);
+// Console.WriteLine($"{string.Join(" ", array)}");
+// int count = 0;
+// for (int i = 0; i < array.Length; i++)
+// {
+//         if (array[i] >= 10 && array[i]<= 99)
+//         {
+//                 count++;
+//         }
+// }
+// Console.WriteLine(count);
+
+
+// Задача 37: Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+
+//  int[] array = CreateRandomArray(12, -200, 200);
+// Console.WriteLine($"{string.Join(" ", array)}");
+// for (int i = 0; i < array.Length; i++)
+// {
+        
+// }
