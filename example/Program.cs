0fc6e2d20c6a24c7d221098d2cfe40b53c72d686
Akula_tony <konivchenko94@gmail.com>
2022-11-14 20:31:02 +0300

    задачи с семинаров

--- example/Program.cs
+++ example/Program.cs
@@ -416,3 +416,45 @@
 // {
         
 // }
+
+
+
+
+//Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. 
+//Результат запишите в новом массиве.
+
+// Clear();
+// Write("Введите элементы массива через пробел: ");
+// string elements = ReadLine();
+// int[] baseArray = GetArrayFromString(elements);
+
+// WriteLine(String.Join(" ", ResultArray(baseArray)));
+
+// int[] GetArrayFromString(string stringArray)
+// {
+//     string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+//     int[] res = new int[nums.Length];
+
+//     for (int i = 0; i < nums.Length; i++)
+//     {
+//         res[i] = int.Parse(nums[i]);
+//     }
+//     return res;
+// }
+
+// int[] ResultArray(int[] array)
+// {
+//     int size = array.Length / 2;
+//     if (array.Length % 2 == 1) size++;
+//     int[] result = new int[size];
+//     for (int i = 0; i < array.Length / 2; i++)
+//     {
+        
+//         result[i] = array[i] * array[array.Length - 1 - i];
+//     }
+//     if (array.Length % 2 == 1) result[size-1] = array[array.Length / 2];
+//     return result;
+// }
+
+
+
