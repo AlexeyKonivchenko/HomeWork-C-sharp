566fa1f0b61707c8f63a1b4ebce989b5766f4ead
AlexKon <konivchenko94@gmail.com>
2022-11-27 22:11:23 +0300

    Задача № 64

---
+++ HWLess9/task1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+//  Выполнить с помощью рекурсии.
+
+
+Console.Clear();
+
+Console.WriteLine("Введите первое число:");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 1) Console.WriteLine("Число должно быть больше 1.");
+else Shownumbers(n);
+
+void Shownumbers(int n)
+{
+    Console.Write($"{n} ");
+    if (n == 1) return;
+    Shownumbers(n - 1);
+}
