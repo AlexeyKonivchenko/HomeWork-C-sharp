99b6a569e35a41f45831150a07f04963f330e358
AlexKon <konivchenko94@gmail.com>
2022-11-28 14:43:23 +0300

    Задача № 66

---
+++ HWLess9/task2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+Console.Clear();
+
+
+Console.WriteLine("Введите первое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (m > n)
+{
+    Console.WriteLine("Необходимо поменять местами числа!");
+    Console.Write($"Результат между {n} и {m} = ");
+    SumBetweenNumbers(n, m, 0);
+}
+else
+{
+    Console.Write($"Результат между {n} и {m} = ");
+    SumBetweenNumbers(m, n, 0);
+}
+
+void SumBetweenNumbers(int start, int end, int sum)
+{
+    sum += end;
+    if (end <= start)
+    {
+        Console.WriteLine($" {sum} ");
+        return;
+    }
+    SumBetweenNumbers(start, end - 1, sum);
+}
